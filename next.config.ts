46afbf8e4e229a3ef502a5806147f5939d05ebb2
DavidPonce-dev <e.chami.tap@gmail.com>
2026-06-09 04:40:58 -0700

    fix: allow cloudflareinsights beacon in CSP script-src

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com https://static.cloudflareinsights.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: https://i.ytimg.com",
       "font-src 'self' https://fonts.gstatic.com",
